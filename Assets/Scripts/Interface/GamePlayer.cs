ae3cd42582665ca709c8f2d3ac578caa6a99d887
Ebimonaca <74030378+youse19999@users.noreply.github.com>
2026-07-02 10:10:22 +0900

    コンフリクト修正

--- Assets/Scripts/Interface/GamePlayer.cs
+++ Assets/Scripts/Interface/GamePlayer.cs
@@ -1,17 +1,13 @@
-<<<<<<< Updated upstream
 =======
-using NUnit.Framework.Interfaces;
 using System.Data.Common;
->>>>>>> Stashed changes
+>>>>>>> main
+using NUnit.Framework.Interfaces;
+using System.Drawing;
 using UnityEngine;
 
 public class GamePlayer : MonoBehaviour, IPlayer
 {
-<<<<<<< Updated upstream
-    public void SetPosition()
-    {
-=======
-    
+
     private Vector3 point;
 
     private MeshFilter itemFilter;
@@ -19,74 +15,90 @@ public class GamePlayer : MonoBehaviour, IPlayer
     {
 
     }
->>>>>>> Stashed changes
 
+}
+public void Start()
+{
+    GetRigidBody();
+    itemFilter.mesh = GameStructure.GetInstance().playerStructure.Item.mesh;
+}
+public void Update()
+{
+    RaycastHit hit;
+    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+    {
+        point = hit.point;
+        if (Vector2.Distance(point, this.transform.position) < GameStructure.GetInstance().playerStructure.deadzone)
+        {
+            GetRigidBody().linearVelocity = Vector3.zero;
+            return;
+        }
+        if (hit.collider.gameObject == this.gameObject)
+        {
+            GetRigidBody().linearVelocity = Vector3.zero;
+            return;
+        }
+        this.transform.rotation = Quaternion.LookRotation(point - this.transform.position, this.transform.forward);
+        //ツイスト
+        Quaternion refQ = this.transform.rotation;
+        Vector3 qVector = new Vector3(refQ.x, refQ.y, refQ.z);
+        //yのみ
+        Vector3 projection = Vector3.Project(qVector, Vector3.up);
+        //クオータニオン
+        Quaternion q = new Quaternion(projection.x, projection.y, projection.z, this.transform.rotation.w);
+        //符号
+        float dot = Quaternion.Dot(q, refQ);
+        if (dot > 0.0f)
+        {
+            this.transform.rotation = q;
+        }
+        else
+        {
+            this.transform.rotation = Quaternion.Inverse(q);
+        }
+        //Yのaxisの入力
+        float verticalInput = Input.GetAxis("Vertical");
+        //使うスピード（input依存)
+        float maxSpeed = GameStructure.GetInstance().playerStructure.speed * verticalInput;
+        //ベロシティー
+        Vector3 velocity = GetRigidBody().linearVelocity + (this.transform.forward * maxSpeed);
+        //クランプ（ここでyもクランプされる）
+        Vector3 clampedVelocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        //yをの要素を復元
+        clampedVelocity.y = velocity.y;
+        //最終的なクランプ済みのlinearVelocityをセット
+        GetRigidBody().linearVelocity = clampedVelocity;
     }
-<<<<<<< Updated upstream
+}
+public void OnDrawGizmos()
+{
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(point, 0.1f);
+    Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.right);
+    Gizmos.color = Color.blue;
+    Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.forward);
+    Gizmos.color = Color.green;
+    Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.up);
+}
 }
 =======
+    public void CreateMove()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected Rigidbody GetRigidBody()
+    {
+        Rigidbody body;
+        if (!this.TryGetComponent<Rigidbody>(out body))
+        {
+            body = this.gameObject.AddComponent<Rigidbody>();
+        }
+        return body;
+    }
     public void Start()
     {
         GetRigidBody();
-        itemFilter.mesh = GameStructure.GetInstance().playerStructure.Item.mesh;
-    }
-    public void Update()
-    {
-        RaycastHit hit;
-        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
-        {
-            point = hit.point;
-            if (Vector2.Distance(point, this.transform.position) < GameStructure.GetInstance().playerStructure.deadzone)
-            {
-                GetRigidBody().linearVelocity = Vector3.zero;
-                return;
-            }
-            if (hit.collider.gameObject == this.gameObject)
-            {
-                GetRigidBody().linearVelocity = Vector3.zero;
-                return;
-            }
-            this.transform.rotation = Quaternion.LookRotation(point - this.transform.position, this.transform.forward);
-            //ツイスト
-            Quaternion refQ = this.transform.rotation;
-            Vector3 qVector = new Vector3(refQ.x, refQ.y, refQ.z);
-            //yのみ
-            Vector3 projection = Vector3.Project(qVector, Vector3.up);
-            //クオータニオン
-            Quaternion q = new Quaternion(projection.x, projection.y, projection.z, this.transform.rotation.w);
-            //符号
-            float dot = Quaternion.Dot(q, refQ);
-            if (dot > 0.0f)
-            {
-                this.transform.rotation = q;
-            }
-            else
-            {
-                this.transform.rotation = Quaternion.Inverse(q);
-            }
-            //Yのaxisの入力
-            float verticalInput = Input.GetAxis("Vertical");
-            //使うスピード（input依存)
-            float maxSpeed = GameStructure.GetInstance().playerStructure.speed * verticalInput;
-            //ベロシティー
-            Vector3 velocity = GetRigidBody().linearVelocity + (this.transform.forward * maxSpeed);
-            //クランプ（ここでyもクランプされる）
-            Vector3 clampedVelocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-            //yをの要素を復元
-            clampedVelocity.y = velocity.y;
-            //最終的なクランプ済みのlinearVelocityをセット
-            GetRigidBody().linearVelocity = clampedVelocity;
-        }
-    }
-    public void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(point, 0.1f);
-        Gizmos.DrawLine(this.transform.position, this.transform.position+this.transform.right);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.forward);
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.up);
     }
 }
->>>>>>> Stashed changes
+>>>>>>> main
