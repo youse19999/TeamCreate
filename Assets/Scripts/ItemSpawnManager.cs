084a6d7f8c79b3f6355a3215c353868492d43a28
Ito-Michinobu <itoitoito893@gmail.com>
2026-07-16 10:04:10 +0900

    ポイントにstatic追加

--- Assets/Scripts/ItemSpawnManager.cs
+++ Assets/Scripts/ItemSpawnManager.cs
@@ -5,9 +5,10 @@ public class ItemSpawnManager : MonoBehaviour
 {
     [SerializeField] private float spawnInterval = 10.0f;//スポーン感覚
     [SerializeField] private int maxSpawnAmount = 3;//マップ内に存在できるアイテムの数
-    private int currentSpawnAmount = 0;
+    public static int currentSpawnAmount = 0;
     [SerializeField] BoxCollider spawnArea;
-    public List<GameObject> ItemList;
+    public List<GameObject> ItemList;//ノーマルアイテム
+    public List<GameObject> SupecialItemList;//スペシャルアイテム
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,6 +22,7 @@ public class ItemSpawnManager : MonoBehaviour
         ItemSpawn();
     }
 
+    //スポーンの条件
     void ItemSpawn()
     {
         if (ItemList == null) { return; }
@@ -32,20 +34,34 @@ public class ItemSpawnManager : MonoBehaviour
 
         //スポーン条件
         //等間隔でSpawnAreaのランダムな位置にスポーン
-        //if ()
-        //{
-        //
-        //}
+ 
         //マップ内のアイテム０ならスポーン
         if(currentSpawnAmount == 0)
         {
-           
+            SpawnItem();
         }
         //制限時間が一定以下になると10pアイテムのスポーン
         //if(制限時間が一定を下回ったら)
         {
             Instantiate(ItemList[ItemList.Count], spawnArea.transform.position, Quaternion.identity);
         }
+    }
+
+    //スポーン処理
+    void SpawnItem()
+    {
+        //スポーンエリア内のランダムな位置取得
+        Bounds bounds = spawnArea.bounds;
+
+        float randomX = Random.Range(bounds.min.x, bounds.max.x);
+        float randomY = Random.Range(bounds.min.y, bounds.max.y);
+        float randomZ = Random.Range(bounds.min.z, bounds.max.z);
+
+        Vector3 randomPosition = new Vector3(randomX, randomY, randomZ);
+
+        //アイテムのスポーン
+        int randamItemNumber = Random.Range(0, ItemList.Count - 2);
+        Instantiate(ItemList[randamItemNumber], randomPosition, Quaternion.identity);
     }
 
 }
