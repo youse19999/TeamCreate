6b8f80bca18c6eeddb0068c260eb218266345ccf
kou <namekoziru63@gmail.com>
2026-07-16 11:48:50 +0900

    UIScriptの修正とCotrolSceneからmainへScane遷移の追加

--- Assets/Scripts/PointCanvas.cs
+++ Assets/Scripts/PointCanvas.cs
@@ -1,10 +1,15 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PointCanvas : MonoBehaviour
 {
     public TMP_Text ScoreText1P;
     public TMP_Text ScoreText2P;
+    [SerializeField] private GameObject OneP;
+    [SerializeField] private GameObject TwoP;
+    GoalArea OnePlayer;
+    GoalArea TwoPlayer;
 
     //1Pと2Pの値を入れ、それをTEXTで表示
     public void Point(int OneP,int TwoP)
@@ -16,14 +21,14 @@ public class PointCanvas : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        OnePlayer = OneP.GetComponent<GoalArea>();
+        TwoPlayer = TwoP.GetComponent<GoalArea>();
         Debug.Log("Start");//デバック用
     }
 
     // Update is called once per frame
     void Update()
     {
-        int kari = 0;//いったん仮
-
-        Point(kari,kari);
+        Point(OnePlayer.point, TwoPlayer.point);
     }
 }
